21d427b8fd333f010da753c05b1feb7bd315fe0b
wpbonelli <wbonelli@ucar.edu>
2024-09-04 20:25:07 -0400

    remove old file

--- flopy4/parsers.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-from lark import Lark
-
-DFN_GRAMMAR_FILE = Path(__file__).parent / "dfn.lark"
-MF6_GRAMMAR_FILE = Path(__file__).parent / "mf6.lark"
-
-DFN_PARSER = Lark(open(DFN_GRAMMAR_FILE).read(), start="value")
-MF6_PARSER = Lark(open(MF6_GRAMMAR_FILE).read(), start="value")
