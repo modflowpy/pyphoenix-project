8fbee067549e7d0a056e22e227f4ef1be360d699
wpbonelli <wbonelli@ucar.edu>
2025-02-19 10:33:57 -0500

    only beartype if developing, update uv lockfile

--- flopy4/__init__.py
+++ flopy4/__init__.py
@@ -1,3 +1,9 @@
+import json
+from importlib.metadata import Distribution
+
 from beartype.claw import beartype_this_package
 
-beartype_this_package()
+_PKG_URL = Distribution.from_name("flopy4").read_text("direct_url.json")
+_EDITABLE = json.loads(_PKG_URL).get("dir_info", {}).get("editable", False)
+if _EDITABLE:
+    beartype_this_package()
