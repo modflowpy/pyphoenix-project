98ba19ac5df25ed16635610b2794513afd00acef
mjreno <renomik@gmail.com>
2025-04-29 09:10:29 -0400

    baseline plotting for quickstart (#118)
    Co-authored-by: mjreno <mjreno@IGSAAA071L01144.gs.doi.net>

--- docs/examples/quickstart.py
+++ docs/examples/quickstart.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+
+import flopy
+import matplotlib.pyplot as plt
 import numpy as np
 
 from flopy4.mf6.gwf import Chd, Dis, Gwf, Ic, Npf, Oc
@@ -37,3 +41,43 @@ assert dis.data.botm.sel(lay=0, col=0, row=0) == 0.0
 # check OC
 assert oc.data["save_head"][0] == "all"
 assert oc.data.save_head.sel(per=0) == "all"
+
+# PLOT
+# create budget reader
+bpth = Path("./quickstart_data/mymodel.bud")
+bobj = flopy.utils.CellBudgetFile(bpth, precision="double")
+
+# set specific discharge
+spdis = bobj.get_data(text="DATA-SPDIS")[0]
+
+# create head reader
+hpth = Path("./quickstart_data/mymodel.hds")
+hobj = flopy.utils.HeadFile(hpth, precision="double")
+
+# set heads
+heads = hobj.get_alldata()
+
+# create grid
+grbpth = Path("./quickstart_data/mymodel.dis.grb")
+grid = flopy.discretization.StructuredGrid.from_binary_grid_file(grbpth)
+
+# TODO: get_specific_discharge is dependent on flopy3 model
+# qx, qy, qz = flopy.utils.postprocessing.get_specific_discharge(spdis, gwf)
+
+# set discharge component arrays
+u = []
+v = []
+for r in spdis:
+    u.append(r[3])
+    v.append(r[4])
+qx = np.array(u).reshape(grid.nrow, grid.ncol)
+qy = np.array(v).reshape(grid.nrow, grid.ncol)
+
+fig, ax = plt.subplots()
+pmv = flopy.plot.PlotMapView(modelgrid=grid, ax=ax)
+pmv.plot_array(heads[0][0])
+pmv.plot_grid(colors="white")
+pmv.contour_array(heads[0][0], levels=[0.2, 0.4, 0.6, 0.8], linewidths=3.0)
+pmv.plot_vector(qx, qy, normalize=True, color="white")
+qs_pth = Path("./image/quickstart.png")
+fig.savefig(qs_pth)
