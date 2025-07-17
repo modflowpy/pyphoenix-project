1401aed6f19c174ff7aee12e936e7b9bdcd6dad5
deltamarnix <deltamarnix@users.noreply.github.com>
2025-07-17 11:53:37 +0200

    sim- are packages, not models

--- codegen/make.py
+++ codegen/make.py
@@ -58,7 +58,7 @@ def make_targets(dfn, outdir: PathLike, verbose: bool = False):
         parent = dfn.get("parent", None)
         if parent is None:
             return "simulation.py.jinja"
-        elif parent == "sim":
+        elif parent == "sim" and not dfn["name"].startswith("sim-"):
             return "model.py.jinja"
         else:
             return "package.py.jinja"
