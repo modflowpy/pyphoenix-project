0e5f15cf1f00e9f8e2dd1392fb30f807057da059
mjreno <mjreno@IGSAAA071L01144.gs.doi.net>
2025-10-09 22:51:56 -0400

    fix slntype

--- flopy4/mf6/solution.py
+++ flopy4/mf6/solution.py
@@ -1,6 +1,6 @@
 from abc import ABC
 from pathlib import Path
-from typing import Optional
+from typing import ClassVar, Optional
 
 import attrs
 from xattree import field, xattree
@@ -10,9 +10,10 @@ from flopy4.mf6.package import Package
 
 @xattree
 class Solution(Package, ABC):
+    slntype: ClassVar[str] = "sln"
+
     slnfname: Optional[Path] = field(default=None)  # type: ignore
     models: list[str] = attrs.field(default=attrs.Factory(list))
 
     def default_filename(self) -> str:
-        ftype = self.slntype.lower() if self.slntype else "sln"
-        return str(self.slnfname) if self.slnfname else f"solution.{ftype}"
+        return str(self.slnfname) if self.slnfname else f"solution.{self.slntype.lower()}"
