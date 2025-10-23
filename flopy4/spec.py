b2d2fab2a75d360b8df323701799d071fc51a667
w-bonelli <wesbonelli@gmail.com>
2025-10-23 18:45:45 -0400

    cleanup todo comment

--- flopy4/spec.py
+++ flopy4/spec.py
@@ -1,8 +1,6 @@
 """
 Wrap `xattree` and `attrs` specification utilities.
 These include field decorators and introspection functions.
-TODO: add `derived` option to dims? or more generic option
-to any field indicating it is not part of the formal spec?
 """
 
 import numpy as np
