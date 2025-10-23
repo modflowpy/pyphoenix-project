b6fd95b2656db8e78b574c0ac959481bf7342248
w-bonelli <wesbonelli@gmail.com>
2025-10-22 23:22:04 -0400

    oc arrays object -> str dtype, type checking off for now

--- flopy4/mf6/gwf/oc.py
+++ flopy4/mf6/gwf/oc.py
@@ -46,29 +46,25 @@ class Oc(Package):
     )
     # TODO: needs coverter and then rename?
     head: Optional[Format] = field(block="options", default=None)
-    save_head: Optional[NDArray[np.object_]] = array(
-        object,
+    save_head: Optional[NDArray[np.str_]] = array(
         block="period",
         default="all",
         dims=("nper",),
         converter=Converter(dict_to_array, takes_self=True, takes_field=True),
     )
-    save_budget: Optional[NDArray[np.object_]] = array(
-        object,
+    save_budget: Optional[NDArray[np.str_]] = array(
         block="period",
         default="all",
         dims=("nper",),
         converter=Converter(dict_to_array, takes_self=True, takes_field=True),
     )
-    print_head: Optional[NDArray[np.object_]] = array(
-        object,
+    print_head: Optional[NDArray[np.str_]] = array(
         block="period",
         default="all",
         dims=("nper",),
         converter=Converter(dict_to_array, takes_self=True, takes_field=True),
     )
-    print_budget: Optional[NDArray[np.object_]] = array(
-        object,
+    print_budget: Optional[NDArray[np.str_]] = array(
         block="period",
         default="all",
         dims=("nper",),
