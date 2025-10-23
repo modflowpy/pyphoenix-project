9cb90f3ac167e1db77cb9af76441659cc186f8b1
w-bonelli <wesbonelli@gmail.com>
2025-10-23 10:19:19 -0400

    fix tcl flakiness on windows runners

--- test/conftest.py
+++ test/conftest.py
@@ -1,4 +1,8 @@
 from pathlib import Path
+from platform import system
+
+import pytest
+from modflow_devtools.misc import is_in_ci
 
 pytest_plugins = ["modflow_devtools.fixtures"]
 
@@ -6,6 +10,15 @@ PROJ_ROOT_PATH = Path(__file__).parents[1]
 DOCS_PATH = PROJ_ROOT_PATH / "docs"
 EXAMPLES_PATH = DOCS_PATH / "examples"
 EXCLUDED_EXAMPLES = []
+
+
+@pytest.fixture(scope="session", autouse=True)
+def patch_macos_ci_matplotlib():
+    # use noninteractive matplotlib backend if in Mac OS CI to avoid pytest-xdist node failure
+    if is_in_ci() and system().lower() == "darwin":
+        import matplotlib
+
+        matplotlib.use("agg")
 
 
 def pytest_generate_tests(metafunc):
