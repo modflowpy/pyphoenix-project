d70efc3bd1db5a831f02f9c01673e81a51ded561
wpbonelli <wbonelli@ucar.edu>
2026-06-05 07:42:14 -0700

    look for version.txt if syncing with local dfn path

--- flopy4/mf6/_compat.py
+++ flopy4/mf6/_compat.py
@@ -10,7 +10,9 @@ def _query_mf6_version(exe: str) -> str | None:
     try:
         out = subprocess.check_output([exe, "-v"], text=True, stderr=subprocess.STDOUT)
         m = _VERSION_RE.search(out)
-        return m.group(1) if m else None
+        v = m.group(1) if m else None
+        v = v.rpartition("+")[0]  # TODO how to handle vcs tag section?
+        return v
     except Exception:
         return None
 
